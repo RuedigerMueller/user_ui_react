87496c65a6d63aacda6671784815cccabaaee745
RuedigerMueller <unconfigured@null.spigotmc.org>
2022-03-11 14:28:42 +0100

    Very first login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { Login } from './Login';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,30 +1,84 @@
 import React, { ReactNode } from 'react';
+import axios from "axios";
 
 type Props = {};
-type State = {};
+
+const initialState = Object.freeze({
+    username: 'Admin',
+    password: 'changeme',
+});
+
+type State = typeof initialState;
+
+const baseURL = "http://localhost:3001/login";
 
 export class Login extends React.Component<Props, State> {
-    private login() {
-        alert('Login');
+    readonly state = initialState;
+
+    handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.name);
+        console.log(event.target.value);      
+        switch(event.target.name) {
+            case 'password': {
+                this.setState({
+                    password: event.target.value,
+                });
+                break;
+            }
+            case 'username': {
+                this.setState({
+                    username: event.target.value,
+                });
+                break
+            }
+            default: {
+                console.log('unexpected field updated');
+                break;
+            }
+        }
+    }    
+
+    login = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        var config = {
+            headers: {
+                "Content-Type": "application/json",
+            }
+        };
+
+        axios
+            .post(
+                baseURL,
+                {
+                    username: this.state.username,
+                    password: this.state.password,
+                },
+                config)
+            .then(response => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     public render(): ReactNode {
-        return (
-            <div className="login">
-                <form onSubmit={this.login}>
-                    <p>
-                        <label htmlFor="user">User</label>
-                        <input type="text" name="user" id="user"></input>
-                    </p>
-                    <p>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="password"></input>
-                    </p>
-                    <p>
-                        <input type="submit" value="Login"/>
-                    </p>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className="login">
+            <form onSubmit={this.login}>
+                <p>
+                    <label htmlFor="user">User</label>
+                    <input type="text" value={this.state.username} name="username" id="username" onChange={this.handleChange}></input>
+                </p>
+                <p>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" value={this.state.password} name="password" id="password" onChange={this.handleChange}></input>
+                </p>
+                <p>
+                    <input type="submit" value="Login" />
+                </p>
+            </form>
+        </div>
+    );
+}
 }
